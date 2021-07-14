f82fde2ffb5c888f12deb95e4f33ca52169c3248
bipon68 <bipon68@users.noreply.github.com>
2021-07-14 18:41:09 +0600

    #2 es6 var, let, const

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello world</h1>;
-console.log(element)
-ReactDOM.render(element, document.getElementById('root'))
+// const element = <h1>Hello world</h1>;
+// console.log(element)
+// ReactDOM.render(element, document.getElementById('root'))
+
+// #ES6 Refresher 1 - var, let, const
+// var -> function
+// let -> block
+// const -> block
+
+// const x = 1;
+// x = 2;
+
+// function sayHello(){
+//   for (let i = 0; i<5;  i++){
+//     console.log(i)
+//   }
+//   console.log(i)
+// }
+// sayHello();
+
+
+// #ES6 Refresher 2 - Object
 
 // import './index.css';
 // import App from './App';
