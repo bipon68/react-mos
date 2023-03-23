645ba5d6efb519e36c2a2eb4c6c7688a1c5f7371
bipon68 <bipon68@users.noreply.github.com>
2023-03-23 22:00:14 +0600

    #3 Handling Events, State management, Passing Data via props

--- react-18/react-mos-game/src/App.tsx
+++ react-18/react-mos-game/src/App.tsx
@@ -1,9 +1,21 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 import Message from "./Message";
 
 
 function App(){
-  return <div><ListGroup /></div>
+
+  let items = ["Dhaka", "Khulna", "Rajshai", "Albania", "Berlin"];
+  //  items = [];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item)
+  }
+
+  return <div>
+      {/* <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}/> */}
+        <Alert>Hello <span>World</span></Alert>
+    </div>
 }
 
 export default App;
--- react-18/react-mos-game/src/components/ListGroup.tsx
+++ react-18/react-mos-game/src/components/ListGroup.tsx
@@ -1,28 +1,54 @@
-import { Fragment } from "react";
+import { Fragment, MouseEvent, useState } from "react";
 
-function ListGroup() {
-  let items = ["Dhaka", "Khulna", "Rajshai", "Albania", "Berlin"];
-  items = [];
-
-
-//   if(items.length === 0){
-//       return <><h2>List</h2><p>No item found</p></>
-//   }
-
-// const message = items.length === 0 ? <p>No Item Found</p>: null;
-const getMessage = () => {
-    return items.length === 0 ? <p>No Item Found</p>: null;
+interface Props{
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void
 }
+
+function ListGroup({items, heading, onSelectItem}: Props) {
+  // let items = ["Dhaka", "Khulna", "Rajshai", "Albania", "Berlin"];
+  // items = [];
+  // let selectedIndex = -1;
+
+  // if(items.length === 0){
+  //   return <><h2>List</h2><p>No item Found</p></>
+  // }
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  // const message = items.length === 0 ? <p>No Item Found</p>: null;
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p>No Item Found</p> : null;
+  // };
+
+  const [selectedIndex, setselectedIndex] = useState(-1)
+
+
 
   return (
     <>
       <h2>List</h2>
-      {items.length === 0 ? <p>No Item Found</p>: null}
+      {items.length === 0 ? <p>No item found</p>: null}
       {/* {message} */}
       {/* {getMessage()} */}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setselectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
