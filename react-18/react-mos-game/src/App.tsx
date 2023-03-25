c89248805058bd122578340e1f72ac15329988aa
bipon68 <bipon68@users.noreply.github.com>
2023-03-25 19:25:22 +0600

    #5 building a like component

--- react-18/react-mos-game/src/App.tsx
+++ react-18/react-mos-game/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
 import ListGroup from "./components/ListGroup";
 import Message from "./Message";
 import {BsFillCalendar2EventFill} from 'react-icons/bs'
+import Like from "./components/Like";
 
 
 
@@ -19,7 +20,8 @@ function App(){
   const [alertVisible, setAlertVisible] = useState(false);
 
   return <div>
-      <BsFillCalendar2EventFill color="red" size="40"/>
+      {/* <BsFillCalendar2EventFill color="red" size="40"/> */}
+      <Like onClick={() => console.log('Clicked')} />
       <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}/>
         {/* {alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
         <Button  onClick={() => setAlertVisible(true)}>My Button</Button> */}
--- react-18/react-mos-game/src/components/Button/Button.tsx
+++ react-18/react-mos-game/src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import React, { Children } from 'react'
+import styles from '/Button.module.css'
 
 interface Props{
     children: string;
