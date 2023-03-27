6ba040f67100a402dc5f8973079ee8859c51eae9
bipon68 <bipon68@users.noreply.github.com>
2023-03-27 12:47:32 +0600

    #7 accessing input field

--- react-18/react-mos-game/src/Form.tsx
+++ react-18/react-mos-game/src/Form.tsx
@@ -1,10 +1,21 @@
 import React, { FormEvent } from "react";
+import { useRef } from "react";
 
 const Form = () => {
 
+    const nameRef = useRef<HTMLInputElement>(null);
+    const ageRef = useRef<HTMLInputElement>(null)
+    const person = {name: '', age: 0}
+
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
-        console.log('Submitted')
+        if(nameRef.current !==null)
+            person.name = nameRef.current.value;
+            // console.log(nameRef.current.value)
+        if(ageRef.current !==null)
+            person.age = parseInt(ageRef.current.value)
+            // console.log(ageRef.current.value)
+        console.log(person)
     }
 
   return (
@@ -13,11 +24,11 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">Submit</button>
     </form>
